6bb69abdad0114a5a97a7686109f150b0c2fccb9
Lucca <luccamonnie@gmail.com>
2018-03-24 15:51:19 -0700

    test commit

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20180324210228) do
   create_table "reviews", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.integer "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
