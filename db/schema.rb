a3990a3bb29737c9819def1509bafde3791a6c5a
Nicole Cahlander <ncahlander@pugetsound.edu>
2018-04-13 10:47:00 -0700

    Revert "Bug fix to users controller"
    This reverts commit 63102f0df7fa6c4350eebe7964c7991bd256ec17.

--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20180403041515) do
   create_table "reviews", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.integer "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "numOfThing"
