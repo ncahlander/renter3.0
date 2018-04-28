a55d641107089754ca8de04bbb75580a10b6c7a3
Lucca <luccamonnie@gmail.com>
2018-04-27 20:47:21 -0700

    about page content

--- db/schema.rb
+++ db/schema.rb
@@ -101,8 +101,8 @@ ActiveRecord::Schema.define(version: 20180428012540) do
     t.string "username"
     t.string "firstName"
     t.string "lastName"
-    t.boolean "admin", default: false
     t.string "role"
+    t.boolean "admin", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
